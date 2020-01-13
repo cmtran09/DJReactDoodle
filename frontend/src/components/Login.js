7bb0ba9f0d21a723e8a2cfb6c7e5c157ee27707b
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-01-13 15:27:51 +0000

    register and login

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,159 @@
+
+import React, { useState } from 'react'
+import axios from 'axios'
+import Auth from '../lib/auth'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import Link from '@material-ui/core/Link'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+
+const initialLoginState = {
+  username: '',
+  email: '',
+  password: ''
+}
+
+const errorInitialState = {
+  errors: ''
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh'
+  },
+  image: {
+    backgroundImage: 'url(https://images.unsplash.com/photo-1494396550299-e4eb26401c8b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2851&q=80)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}))
+
+const Login = (props) => {
+
+  const classes = useStyles()
+
+  const [form, updateForm] = useState(initialLoginState)
+  const [error, setError] = useState(errorInitialState)
+
+  function handleInput(e) {
+    updateForm({ ...form, [e.target.name]: e.target.value })
+    setError({ ...error, errors: '' })
+    console.log(form)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!form) return
+    axios.post('/api/login', form)
+      .then(resp => {
+        Auth.setToken(resp.data.token)
+        console.log(resp.data.token)
+      })
+      .then(() => props.history.push('/spots'))
+      .catch((err) => setError({ errors: 'Email or Password Incorrect' }))
+  }
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form}  onSubmit={(e) => handleSubmit(e)}>
+            <TextField
+              // variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              type="text"
+              label="username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              onChange={(e) => handleInput(e)}
+            />
+            <TextField
+              // variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              type="text"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={(e) => handleInput(e)}
+            />
+            <TextField
+              // variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              type="text"
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(e) => handleInput(e)}
+            />
+            {error.errors && <small className="help is-danger">{error.errors}</small>}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              // onClick={console.log('hi')}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="/register" variant="body2">
+                  {'Don\'t have an account? Join here'}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Login
